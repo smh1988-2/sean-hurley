ae21a86d988b3bf3d2f0ae7a8e73f3391e79ffff
smh1988-2 <seamhurley1988@gmail.com>
2023-12-06 15:53:54 -0800

    updated project cards styling

--- app/components/Cards/ProjectCard/ProjectCard.tsx
+++ app/components/Cards/ProjectCard/ProjectCard.tsx
@@ -1,21 +1,30 @@
-import React from 'react'
+import React from "react";
 
-import styles from "./projectCard.module.css"
-import Image from 'next/image'
+import styles from "./projectCard.module.css";
+import { work_sans } from "@/app/fonts";
+
+import Image from "next/image";
 
 function ProjectCard() {
   return (
     <div className={styles.projectCard}>
-        <h3 className={styles.projectCardTitle}>title</h3>
-        <p className={styles.projectCardDescription}>description</p>
-        <div className={styles.toolsContainer}>
-            <p className={styles.projectCardTool}>Tool 1</p>
-            <p>Tool 2</p>
-        </div>
+      <h3 className={`${work_sans.className} ${styles.projectCardTitle}`}>
+        title
+      </h3>
+      <p className={styles.projectCardDescription}>description</p>
+      <div className={styles.toolsContainer}>
+        <p className={styles.projectCardTool}>Tool 1</p>
+        <p className={styles.projectCardTool}>Tool 2</p>
+      </div>
 
-        <a href="" className={styles.projectCardLink}>View Project</a>
-        </div>
-  )
+      <div className={styles.projectLinkContainer}>
+      <a href="" className={`${work_sans.className} ${styles.projectCardLink}`}>
+        View Project
+      </a>
+      <span className={styles.projectArrow}></span>
+      </div>
+    </div>
+  );
 }
 
-export default ProjectCard
+export default ProjectCard;
