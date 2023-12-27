8eb8d2e505a4358c2721d134d66cb36fc52cc14f
smh1988-2 <seamhurley1988@gmail.com>
2023-12-27 14:51:50 -0800

    callout setup

--- app/components/Callouts/Callouts.tsx
+++ app/components/Callouts/Callouts.tsx
@@ -3,17 +3,41 @@ import React from "react";
 import { work_sans } from "@/app/fonts";
 
 import styles from "./callouts.module.css";
+import CalloutCard from "./CalloutCard";
 
 function Callouts() {
   return (
     <section className={styles.calloutsSection}>
-      <div className={`container ${styles.conversionPanelContainer}`}>
+      <div className={`container ${styles.calloutContainer}`}>
         <h2
-         className={`section-heading section-heading-centered ${work_sans.className}`}
+          className={`section-heading section-heading-centered ${work_sans.className}`}
         >
           I'm proud of the work I've done 🏆
         </h2>
 
+        <div className={styles.calloutWrapper}>
+          <CalloutCard
+            color={"green"}
+            content={
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eget arcu scelerisque, maximus lorem ac, aliquet odio. Nunc non blandit mi. Etiam tristique nulla tellus, ut condimentum sapien ullamcorper at."
+            }
+            alignment={"left"}
+          />
+          <CalloutCard
+            color={"purple"}
+            content={
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eget arcu scelerisque, maximus lorem ac, aliquet odio. Nunc non blandit mi. Etiam tristique nulla tellus, ut condimentum sapien ullamcorper at."
+            }
+            alignment={"right"}
+          />
+          <CalloutCard
+            color={"yellow"}
+            content={
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eget arcu scelerisque, maximus lorem ac, aliquet odio. Nunc non blandit mi. Etiam tristique nulla tellus, ut condimentum sapien ullamcorper at."
+            }
+            alignment={"left"}
+          />
+        </div>
       </div>
     </section>
   );
